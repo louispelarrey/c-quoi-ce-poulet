5a90d2bcd5e34cbcdb1283453aec014ac0bd8ebd
louispelarrey <louispelarrey@gmail.com>
2023-01-17 12:57:12 +0100

    added automatic build of all app with bash script

---
+++ install.sh
@@ -0,0 +1,13 @@
+cd thematic-api
+docker compose build --pull --no-cache
+docker compose up -d
+docker compose exec php sh -c '
+    set -e
+    apk add openssl
+    php bin/console lexik:jwt:generate-keypair
+    setfacl -R -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
+    setfacl -dR -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
+'
+cd ..
+cd thematic-front
+docker compose up -d
--- thematic-front/vite.config.js
+++ thematic-front/vite.config.js
@@ -4,14 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  "plugins": [
-    eslint({
-      "include": [
-        "sources/**/*.jsx",
-        "sources/**/*.js"
-      ]
-    })
-  ],
   "server": {
     "port": 8000,
     "host": "0.0.0.0"
