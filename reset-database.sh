31299f053ae77142e6439d67a6f3c248ed39569c
louispelarrey <louispelarrey@gmail.com>
2023-01-18 11:24:53 +0100

    changed reset database

--- reset-database.sh
+++
@@ -1,4 +0,0 @@
-php bin/console d:d:d --force
-php bin/console d:d:c
-php bin/console d:s:u --force
-php bin/console d:f:l --no-interaction
---
+++ thematic-api/reset-database.sh
@@ -0,0 +1,4 @@
+docker compose exec php bin/console d:d:d --force
+docker compose exec php bin/console d:d:c
+docker compose exec php bin/console d:s:u --force
+docker compose exec php bin/console d:f:l --no-interaction
