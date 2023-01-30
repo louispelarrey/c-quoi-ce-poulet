6eef5d5574e029ffc7f2bd553af90a4a9f90403e
louispelarrey <louispelarrey@gmail.com>
2023-01-30 22:57:40 +0100

    a

--- thematic-api/deploy.sh
+++ thematic-api/deploy.sh
@@ -1,5 +1,5 @@
-SERVER_NAME=your-domain-name.example.com \
-APP_SECRET=ChangeMe \
+SERVER_NAME=cquoicepoulet.com \
+APP_SECRET=89NC75ZAN0PQQ348FHS8f \
 POSTGRES_PASSWORD=ChangeMe \
 CADDY_MERCURE_JWT_SECRET=ChangeMe \
 docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
