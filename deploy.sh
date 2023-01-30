823d32ef9a487740c67475f2b6e6ad0e5f4f228c
louispelarrey <louispelarrey@gmail.com>
2023-01-30 23:43:23 +0100

    force recreate

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ SERVER_NAME=cquoicepoulet.com \
 APP_SECRET=8b36bb475e22928f84Baz0c4d9d6b4a0ecc \
 POSTGRES_PASSWORD=lepoul3tctr0bon93 \
 CADDY_MERCURE_JWT_SECRET=lepoul3tctr0bon93 \
-docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --force-recreate --build
