7f5672d0b776454ed948d0f424d0bbc498118e02
Arthur-creator <arthur290799@gmail.com>
2023-02-13 21:51:06 +0100

    add report feature

--- thematic-front/src/components/Nav.vue
+++ thematic-front/src/components/Nav.vue
@@ -73,6 +73,7 @@ const closePopup = () => {
           </router-link>
         </button>
         <button
+            v-if="userRoles.includes('ROLE_USER')"
             @click="cartOpen = true"
             class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
         >
